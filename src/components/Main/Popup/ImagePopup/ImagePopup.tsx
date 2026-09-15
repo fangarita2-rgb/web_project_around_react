414f6d8c1be1673ced7699162304f0aa3592d16e
Francisco <fangarita2@gmail.com>
2026-09-15 17:36:56 -0500

    fix: corrige eslint.config.js (configs.flat no existe en v5.2) y limpia wrapper de ImagePopup

--- src/components/Main/Popup/ImagePopup/ImagePopup.tsx
+++ src/components/Main/Popup/ImagePopup/ImagePopup.tsx
@@ -6,9 +6,9 @@ type ImagePopupProps = {
 
 export default function ImagePopup({ card }: ImagePopupProps): React.JSX.Element {
   return (
-    <div className="popup__image-container">
+    <>
       <img className="popup__image" src={card.link} alt={card.name} />
       <p className="popup__caption">{card.name}</p>
-    </div>
+    </>
   );
 }
